9fcc8fd688688bd320b462ef2f6092838e276c8b
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-01-11 01:27:51 +0200

    improve design

--- desktop/src/components/AudioInput.tsx
+++ desktop/src/components/AudioInput.tsx
@@ -1,18 +1,17 @@
-import { convertFileSrc } from '@tauri-apps/api/tauri';
-import { useRef, useState } from 'react';
-import { open } from '@tauri-apps/api/dialog';
-import PlayIcon from '../icons/Play';
-import PauseIcon from '../icons/Pause';
+import { convertFileSrc } from "@tauri-apps/api/tauri";
+import { useRef, useState } from "react";
+import { open } from "@tauri-apps/api/dialog";
+import PlayIcon from "../icons/Play";
+import PauseIcon from "../icons/Pause";
 
 export default function AudioInput({
   onChange,
 }: {
   onChange: (path: string) => void;
 }) {
-    const audioRef = useRef<HTMLAudioElement>()
-    const [playing, setPlaying] = useState(false)
-    const [name, setName] = useState<string | null>("");
-
+  const audioRef = useRef<HTMLAudioElement>();
+  const [playing, setPlaying] = useState(false);
+  const [name, setName] = useState<string | null>("");
 
   async function select() {
     const selected = await open({
@@ -24,42 +23,47 @@ export default function AudioInput({
         },
       ],
     });
-    if (selected)  {
-        const resourcePath = convertFileSrc(selected as string);
-        audioRef.current = new Audio(resourcePath as string);
-        setName(selected as string);
-        onChange(selected as string)
+    if (selected) {
+      const resourcePath = convertFileSrc(selected as string);
+      audioRef.current = new Audio(resourcePath as string);
+      setName(selected as string);
+      onChange(selected as string);
     }
-
   }
 
   function onEnd() {
-    setPlaying(false)
+    console.log('end')
+    setPlaying(false);
   }
 
   function play() {
-    audioRef?.current?.play()
-    setPlaying(true)
-    audioRef?.current?.addEventListener('ended', onEnd)
+    audioRef?.current?.play();
+    setPlaying(true);
+    audioRef?.current?.addEventListener("ended", onEnd);
   }
-  
+
   function pause() {
-    audioRef?.current?.pause()
-    setPlaying(false)
+    audioRef?.current?.pause();
+    setPlaying(false);
   }
 
   if (!name) {
     return (
-        <div className='flex items-center w-full justify-center'>
-            <button onClick={select} className="btn btn-primary w-full">Select Audio File</button>
-        </div>
-    )
+      <div className="flex items-center w-full justify-center">
+        <button onClick={select} className="btn btn-primary w-full">
+          Select Audio File
+        </button>
+      </div>
+    );
   }
 
   return (
-    <div className='flex-row  h-10 shadow-lg flex justify-between items-center bg-base-200 rounded-lg w-[100%] m-auto px-3 my-3'>
-        <p>{name?.split('\\').pop()}</p>
-        {playing && <PauseIcon onClick={pause} /> || <PlayIcon onClick={play} />}        
+    <div onClick={() => playing ? pause() : play()} className="flex-row cursor-pointer h-10 shadow-lg flex justify-between items-center bg-base-200 rounded-lg w-[100%] m-auto px-3 my-3 select-none">
+      <p>{name?.split("\\").pop()}</p>
+      <label className={`swap text-9xl ${playing ? 'swap-active' : ''}`}>
+        <div className="swap-off"><PlayIcon /></div>
+        <div className="swap-on"><PauseIcon /></div>
+      </label>
     </div>
   );
 }
--- desktop/src/icons/Pause.tsx
+++ desktop/src/icons/Pause.tsx
@@ -10,8 +10,8 @@ export default function PauseIcon(props: React.SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
       />
     </svg>
