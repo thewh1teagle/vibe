2b41a853cacafaea1138f6db3bf6f9933849e1e5
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-01-14 04:05:12 +0200

    body dir by lang

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,4 +1,5 @@
 import "@fontsource/roboto";
+import { useTranslation } from "react-i18next";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./i18n";
@@ -6,6 +7,9 @@ import SetupPage from "./pages/SetupPage";
 import TranscribePage from "./pages/TranscribePage";
 
 export default function App() {
+  const { i18n } = useTranslation();
+  document.body.dir = i18n.dir();
+
   return (
     <Routes>
       <Route path="/setup" element={<SetupPage />} />
