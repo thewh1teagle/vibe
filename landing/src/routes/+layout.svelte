19c317e2ee9d7a71b67451539735d783f80fc277
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-02-05 16:21:37 +0200

    fix prerender

--- landing/src/routes/+layout.svelte
+++ landing/src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	export const prerender = true;
 	import '../app.css';
 </script>
 
--- landing/svelte.config.js
+++ landing/svelte.config.js
@@ -7,13 +7,16 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	prerender: {
+		default: true
+	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-	}
+	},
+	
 };
 
 export default config;
