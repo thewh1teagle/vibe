caa879b4f4746c9dd97897af670ab4d6c27bee57
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-04-13 22:37:04 +0300

    remove unused static app

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -1,15 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
 mod cmd;
 mod config;
 mod errors;
-
-pub static APP_ASYNC_STATIC: once_cell::sync::Lazy<tokio::sync::Mutex<Option<tauri::AppHandle>>> =
-    once_cell::sync::Lazy::new(|| tokio::sync::Mutex::new(None));
-pub static APP_STATIC: once_cell::sync::Lazy<std::sync::Mutex<Option<tauri::AppHandle>>> =
-    once_cell::sync::Lazy::new(|| std::sync::Mutex::new(None));
 
 fn main() {
     env_logger::init();
@@ -22,14 +16,6 @@ fn main() {
         .plugin(tauri_plugin_updater::Builder::default().build())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
-            let app_handle = app.app_handle();
-            tauri::async_runtime::block_on(async {
-                *APP_ASYNC_STATIC.lock().await = Some(app_handle.to_owned());
-            });
-            *APP_STATIC.lock().unwrap() = Some(app_handle.to_owned());
-            Ok(())
-        })
         .invoke_handler(tauri::generate_handler![
             cmd::transcribe,
             cmd::download_model,
