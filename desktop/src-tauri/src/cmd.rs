6ce2945b2f0eee5e62c3b398a2c6750ca6dfb3d7
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-04-26 21:23:41 +0300

    add taskbar progress

--- desktop/src-tauri/src/cmd.rs
+++ desktop/src-tauri/src/cmd.rs
@@ -1,10 +1,13 @@
 use crate::config;
-use eyre::{Context, OptionExt, Result};
+use eyre::{Context, ContextCompat, OptionExt, Result};
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     Arc,
 };
-use tauri::Manager;
+use tauri::{
+    window::{ProgressBarState, ProgressBarStatus},
+    Manager,
+};
 use vibe::{model::SegmentCallbackData, transcript::Transcript};
 
 #[tauri::command]
@@ -18,14 +21,29 @@ pub async fn download_model(app_handle: tauri::AppHandle) -> Result<()> {
     let mut downloader = vibe::downloader::Downloader::new();
     log::debug!("Download model invoked! with path {}", model_path.display());
 
+    let app_handle_c = app_handle.clone();
     let download_progress_callback = move |current: u64, total: u64| {
-        let window: tauri::WebviewWindow = app_handle.get_webview_window("main").unwrap();
+        let window: tauri::WebviewWindow = app_handle_c.get_webview_window("main").unwrap();
+        let percentage = current / total * 100;
+        window
+            .set_progress_bar(ProgressBarState {
+                progress: Some(percentage),
+                status: Some(ProgressBarStatus::Indeterminate),
+            })
+            .unwrap();
         window.emit("download_progress", (current, total)).unwrap();
         async {}
     };
     downloader
         .download(config::URL, model_path.to_owned(), download_progress_callback)
         .await?;
+    let window = app_handle.get_webview_window("main").unwrap();
+    window
+        .set_progress_bar(ProgressBarState {
+            progress: None,
+            status: Some(ProgressBarStatus::None),
+        })
+        .unwrap();
     Ok(())
 }
 
@@ -54,15 +72,30 @@ pub async fn transcribe(app_handle: tauri::AppHandle, options: vibe::config::Mod
     let abort_atomic_c = abort_atomic.clone();
 
     // allow abort transcription
+    let app_handle_c = app_handle.clone();
     app_handle.listen("abort_transcribe", move |_| {
+        let window = app_handle_c.get_webview_window("main").context("get window").unwrap();
+        window
+            .set_progress_bar(ProgressBarState {
+                progress: None,
+                status: Some(ProgressBarStatus::None),
+            })
+            .unwrap();
         abort_atomic_c.store(true, Ordering::Relaxed);
     });
 
     let abort_callback = move || abort_atomic.load(Ordering::Relaxed);
 
+    let app_handle_c = app_handle.clone();
     let progress_callback = move |progress: i32| {
         // log::debug!("desktop progress is {}", progress);
-        let window = app_handle.get_webview_window("main").unwrap();
+        let window = app_handle_c.get_webview_window("main").unwrap();
+        window
+            .set_progress_bar(ProgressBarState {
+                progress: Some(progress.try_into().unwrap()),
+                status: Some(ProgressBarStatus::Indeterminate),
+            })
+            .unwrap();
         window.emit("transcribe_progress", progress).unwrap();
     };
 
@@ -73,5 +106,10 @@ pub async fn transcribe(app_handle: tauri::AppHandle, options: vibe::config::Mod
         Some(Box::new(abort_callback)),
     )
     .with_context(|| format!("options: {:?}", options))?;
+    let window = app_handle.get_webview_window("main").context("get window")?;
+    window.set_progress_bar(ProgressBarState {
+        progress: None,
+        status: Some(ProgressBarStatus::None),
+    })?;
     Ok(transcript)
 }
