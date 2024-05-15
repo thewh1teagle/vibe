8a96b84ec3d7a283d3c64b46a9b9787c17ba3c35
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-15 22:48:09 +0300

    add swedish language

--- desktop/src/lib/i18n.ts
+++ desktop/src/lib/i18n.ts
@@ -1,31 +1,32 @@
-import * as fs from "@tauri-apps/plugin-fs";
-import { resolveResource } from "@tauri-apps/api/path";
-import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import resourcesToBackend from "i18next-resources-to-backend";
-import { initReactI18next } from "react-i18next/initReactI18next";
+import * as fs from '@tauri-apps/plugin-fs'
+import { resolveResource } from '@tauri-apps/api/path'
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import resourcesToBackend from 'i18next-resources-to-backend'
+import { initReactI18next } from 'react-i18next/initReactI18next'
 
 export const languages: { [key: string]: string } = {
-    "he-IL": "Hebrew",
-    en: "English",
-    "pt-BR": "Portuguese",
-};
+    'he-IL': 'Hebrew',
+    en: 'English',
+    'pt-BR': 'Portuguese',
+    'sv-SE': 'Swedish',
+}
 
 i18n.use(LanguageDetector)
     .use(initReactI18next)
     .use(
         resourcesToBackend(async (language: string, _namespace: string) => {
-            const file_path = await resolveResource(`./locales/${language}.json`);
-            return JSON.parse(await fs.readTextFile(file_path));
+            const file_path = await resolveResource(`./locales/${language}.json`)
+            return JSON.parse(await fs.readTextFile(file_path))
         })
     )
 
     .init({
         debug: true,
-        fallbackLng: "en",
+        fallbackLng: 'en',
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
         },
-    });
+    })
 
-export default i18n;
+export default i18n
