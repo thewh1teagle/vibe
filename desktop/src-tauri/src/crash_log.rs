2f0e96a1f3e60d7336e03b871c9014a25c996de6
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-15 15:06:29 +0300

    add crash log hook

---
+++ desktop/src-tauri/src/crash_log.rs
@@ -0,0 +1,36 @@
+use std::{panic, path::PathBuf, sync::Arc};
+use tauri::{AppHandle, Manager};
+
+use crate::config;
+
+fn get_log_path(app: &AppHandle) -> PathBuf {
+    let config_path = app.path().app_config_dir().unwrap();
+    let mut log_path = config_path.join(format!("{}.txt", config::LOG_FILENAME_PREFIX));
+    let mut count = 0;
+    while log_path.exists() {
+        log_path = config_path.join(format!("{}{}.txt", config::LOG_FILENAME_PREFIX, count));
+        count += 1;
+    }
+    log_path
+}
+
+pub fn set_crash_hook(app: &AppHandle) {
+    let log_path = get_log_path(app);
+    let log_path = Arc::new(log_path);
+
+    panic::set_hook(Box::new(move |info| {
+        let log_path = log_path.clone();
+        let mut message = String::new();
+        message.push_str(&format!("thread '{}' ", std::thread::current().name().unwrap_or("unknown")));
+        message.push_str(&format!("{}", info));
+        if let Ok(var) = std::env::var("RUST_BACKTRACE") {
+            if var == "1" {
+                let backtrace = std::backtrace::Backtrace::capture();
+                message.push_str(&format!("{}", backtrace));
+            }
+        }
+        eprintln!("{}", message);
+        // do whatever with the message
+        std::fs::write(log_path.as_path(), message).unwrap();
+    }));
+}
