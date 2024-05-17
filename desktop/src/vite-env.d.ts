9c71a4f0c57db80fa52d7cbb9374ac7ea99565fd
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-18 00:20:59 +0300

    feat: allow import svgs

--- desktop/src/vite-env.d.ts
+++ desktop/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+    import * as React from 'react'
+
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>
+
+    const src: string
+    export default src
+}
--- desktop/vite.config.ts
+++ desktop/vite.config.ts
@@ -1,9 +1,17 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-    plugins: [react()],
+    plugins: [
+        react(),
+        svgr({
+            // allow import it as regular react component
+            svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
+            include: '**/*.svg',
+        }),
+    ],
 
     resolve: {
         alias: {
