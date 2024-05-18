03abc3d8019b7d7102dbcd89cde660868f69adfe
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-18 07:36:21 +0300

    refactor

--- desktop/src/pages/transcribe/AppMenu.tsx
+++ desktop/src/pages/transcribe/AppMenu.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { ReactComponent as EllipsisIcon } from '~/icons/ellipsis.svg'
 import { ReactComponent as IndicatorIcon } from '~/icons/update-indicator.svg'
 import { cx } from '~/lib/utils'
+
 interface AppMenuProps {
 	availableUpdate: boolean
 	updateApp: () => void
@@ -30,7 +31,6 @@ export default function AppMenu({ availableUpdate, updateApp, onClickSettings }:
 			dir="ltr">
 			<EllipsisIcon />
 			{availableUpdate && <IndicatorIcon className="w-2 h-2 absolute -top-0.5 left-3" />}
-
 			<div tabIndex={0} className="dropdown-content -translate-x-0.5 -translate-y-1.5 z-[1] menu p-2 shadow bg-base-300 rounded-box w-52">
 				<li onMouseDown={() => onClickSettings()}>
 					<a>{t('common.settings')}</a>
