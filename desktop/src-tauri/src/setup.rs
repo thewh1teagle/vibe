e33f582e121f97e6c4b2aa0c6c01e13b32bb90fb
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-21 03:31:18 +0300

    feat: attach windows terminal if RUST_LOG was set

--- desktop/src-tauri/src/setup.rs
+++ desktop/src-tauri/src/setup.rs
@@ -1,12 +1,18 @@
 use std::sync::Mutex;
 use tauri::{App, Manager};
 
-use crate::crash_log;
+use crate::panic_hook;
 
 pub struct OpenedUrls(pub Mutex<Option<Vec<url::Url>>>);
 
 pub fn setup(app: &App) -> Result<(), Box<dyn std::error::Error>> {
-    crash_log::set_crash_hook(app.app_handle());
+    // Add panic hook
+    panic_hook::set_panic_hook(app.app_handle());
+
+    // Attach console IF:
+    // OS is Windows + RUST_LOG was set + attach-console feature was set + console is available.
+    #[cfg(all(windows, feature = "attach-console"))]
+    crate::attach_console::attach();
 
     app.manage(OpenedUrls(Default::default()));
     #[cfg(any(windows, target_os = "linux"))]
