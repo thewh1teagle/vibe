0de79a1f2f030882aa41a59fbf67863156e8ef78
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-21 04:49:48 +0300

    refactor: add deep link module

--- desktop/src-tauri/src/setup.rs
+++ desktop/src-tauri/src/setup.rs
@@ -1,8 +1,5 @@
-use crate::panic_hook;
-use std::sync::Mutex;
+use crate::{deep_link, panic_hook};
 use tauri::{App, Manager};
-
-pub struct OpenedUrls(pub Mutex<Option<Vec<url::Url>>>);
 
 pub fn setup(app: &App) -> Result<(), Box<dyn std::error::Error>> {
     // Add panic hook
@@ -13,20 +10,7 @@ pub fn setup(app: &App) -> Result<(), Box<dyn std::error::Error>> {
         log::debug!("webview version: {}", version);
     }
 
-    app.manage(OpenedUrls(Default::default()));
-    #[cfg(any(windows, target_os = "linux"))]
-    {
-        // NOTICE: `args` may include URL protocol (`your-app-protocol://`) or arguments (`--`) if app supports them.
-        let mut urls = Vec::new();
-        for arg in std::env::args().skip(1) {
-            if let Ok(url) = url::Url::parse(&arg) {
-                urls.push(url);
-            }
-        }
-
-        if !urls.is_empty() {
-            app.state::<OpenedUrls>().0.lock().unwrap().replace(urls);
-        }
-    }
+    // Add deep links from argv as tauri::State
+    deep_link::create_state(app);
     Ok(())
 }
