e549ca58b5f5223d1794b1c48c02e5e3ded62ae3
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-06-01 01:08:11 +0300

    attach console if cli detected

--- desktop/src-tauri/src/console.rs
+++ desktop/src-tauri/src/console.rs
@@ -1,9 +1,11 @@
 use std::env;
 use windows::Win32::System::Console::{AttachConsole, ATTACH_PARENT_PROCESS};
 
+use crate::cli;
+
 /// Attach to console if available and RUST_LOG was set
 pub fn attach() {
-    if env::var("RUST_LOG").is_ok() {
+    if env::var("RUST_LOG").is_ok() || cli::is_cli_detected() {
         // we ignore the result here because
         // if the app started from a command line, like cmd or powershell,
         // it will attach sucessfully which is what we want
