924bcbef785b85545c47ad239fc047759d916ab6
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-06-04 14:39:12 +0300

    improve pre build

--- scripts/pre_build.js
+++ scripts/pre_build.js
@@ -184,9 +184,6 @@ if (!process.env.GITHUB_ENV) {
 		console.log(`export FFMPEG_DIR="${exports.ffmpeg}"`)
 		console.log(`export WHISPER_METAL_EMBED_LIBRARY=ON`)
 	}
-	if (process.argv.includes('--nvidia')) {
-		console.log(`$env:CUDA_PATH = "${exports.cudaPath}"`)
-	}
 	if (!process.env.GITHUB_ENV) {
 		console.log('bunx tauri build')
 	}
