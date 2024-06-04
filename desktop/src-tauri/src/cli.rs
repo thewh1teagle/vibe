3df7c9107b50649ed290a4f1ee5778578940dd06
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-06-04 15:49:39 +0300

    fix(cli): pass correct language

--- desktop/src-tauri/src/cli.rs
+++ desktop/src-tauri/src/cli.rs
@@ -48,7 +48,7 @@ struct Args {
 
     /// Language to transcribe
     #[arg(short, long, default_value = "english", value_parser = get_possible_languages())]
-    language: Option<String>,
+    language: String,
 
     /// Temperature (default: 0.4)
     #[arg(short, long, default_value = "0.4")]
@@ -103,15 +103,23 @@ fn prepare_model_path(path: &Path) -> PathBuf {
     path.to_path_buf()
 }
 
+fn language_name_to_whisper_lang(name: &str) -> Vec<String> {
+    let languages = include_str!("../../src/assets/whisper-languages.json");
+    let languages: Value = serde_json::from_str(languages_json).unwrap();
+    languages[name].as_str().unwrap().to_string()
+}
+
+
 pub fn run(app: &App) {
     #[cfg(target_os = "macos")]
     crate::dock::set_dock_visible(false);
 
     let args = Args::parse();
+    let lang = language_name_to_whisper_lang(&args.language);
     let mut options = TranscribeOptions {
         path: args.file,
         model_path: args.model,
-        lang: args.language,
+        lang: Some(lang),
         init_prompt: args.init_prompt,
         n_threads: args.n_threads,
         temperature: args.temperature,
