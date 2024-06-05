34bb8c85395bd0c4ea5164027dfc76a1ec9478d7
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-06-05 03:04:42 +0300

    fix landing link

--- landing/src/components/Nav.svelte
+++ landing/src/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 <div class="navbar bg-base-100 lg:flex lg:max-w-[1065px] mt-3 m-auto">
 	<div class="flex-1">
-		<a href="/" class="btn btn-ghost text-sm sm:btn-xs lg:btn-md lg:text-xl">
+		<a href="#top" class="btn btn-ghost text-sm sm:btn-xs lg:btn-md lg:text-xl">
 			<Logo />
 			<span class="opacity-95">Vibe</span>
 		</a>
