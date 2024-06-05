5476a1872cb22854c8c5b65df48cfc9f17189d35
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-06-05 06:10:26 +0300

    fix svlete

--- landing/src/components/Nav.svelte
+++ landing/src/components/Nav.svelte
@@ -7,7 +7,7 @@
 
 <div class="navbar bg-base-100 lg:flex lg:max-w-[1065px] mt-3 m-auto">
 	<div class="flex-1">
-		<a href="/" class="btn btn-ghost text-sm sm:btn-xs lg:btn-md lg:text-xl">
+		<a href={base} class="btn btn-ghost text-sm sm:btn-xs lg:btn-md lg:text-xl">
 			<Logo />
 			<span class="opacity-95">Vibe</span>
 		</a>
