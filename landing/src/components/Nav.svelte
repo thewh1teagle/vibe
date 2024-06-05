faa23df679d742843776a614c0e94737c806c26f
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-06-05 03:02:22 +0300

    add navbar to landing

---
+++ landing/src/components/Nav.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Logo from '~/icons/Logo.svelte'
+</script>
+
+<div class="navbar bg-base-100 lg:flex lg:max-w-[1065px] mt-3 m-auto">
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost text-sm sm:btn-xs lg:btn-md lg:text-xl">
+			<Logo />
+			<span class="opacity-95">Vibe</span>
+		</a>
+	</div>
+	<!-- links -->
+	<div class="flex-none" dir="ltr">
+		<!-- todo: add links and features -->
+	</div>
+</div>
--- landing/src/routes/+layout.svelte
+++ landing/src/routes/+layout.svelte
@@ -2,14 +2,18 @@
 	import '~/app.css'
 	import { i18n } from '$lib/i18n'
 	import { isLoading } from 'svelte-i18next'
+	import Nav from '~/components/Nav.svelte'
 </script>
 
 <div dir={$i18n.dir()}>
-	<div class="mt-24 pb-16">
+	<div>
 		{#if $isLoading}
 			<div></div>
 		{:else}
-			<slot />
+			<Nav />
+			<div class="mt-4 lg:mt-10 pb-16">
+				<slot />
+			</div>
 		{/if}
 	</div>
 </div>
