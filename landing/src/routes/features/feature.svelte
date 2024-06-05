ec1eb7650fa97f339ffadd3b82602e8d14572cc0
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-06-05 06:41:52 +0300

    add features align

--- landing/src/routes/features/feature.svelte
+++ landing/src/routes/features/feature.svelte
@@ -5,6 +5,7 @@
 	export let description: string
 	export let videoURL = ''
 	export let imageURL = ''
+	export let align: string = 'start'
 </script>
 
 <div>
@@ -12,10 +13,26 @@
 	<div class="text-lg opacity-80 mt-6 mb-6">{description}</div>
 
 	{#if imageURL}
-		<img src={`${base}${imageURL}`} alt="Image" class="rounded-lg" />
+		<div class="flex {align}">
+			<img src={`${base}${imageURL}`} alt="Image" class="rounded-lg" />
+		</div>
 	{/if}
 
 	{#if videoURL}
-		<video src={`${base}${videoURL}`} controls class="rounded-lg"></video>
+		<div class="flex items-center {align}">
+			<video src={`${base}${videoURL}`} controls class="rounded-lg"></video>
+		</div>
 	{/if}
 </div>
+
+<style>
+	.start {
+		justify-content: flex-start;
+	}
+	.center {
+		justify-content: center;
+	}
+	.end {
+		justify-content: flex-end;
+	}
+</style>
