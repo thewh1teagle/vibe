999e9aca2d423e51b0a80ec12184143e4ec89aa7
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-06-19 22:02:51 +0300

    handle file opened

--- desktop/src/lib/useSingleInstance.tsx
+++ desktop/src/lib/useSingleInstance.tsx
@@ -1,21 +1,23 @@
 import { useTranslation } from 'react-i18next'
-import { ModifyState, NamedPath } from './utils'
+import { ModifyState, NamedPath, pathToNamedPath } from './utils'
 import { ask } from '@tauri-apps/plugin-dialog'
 import { useEffect } from 'react'
 import { listen } from '@tauri-apps/api/event'
+import { useNavigate } from 'react-router-dom'
+import * as config from '~/lib/config'
 
 interface UseSingleInstanceProps {
 	files: NamedPath[]
 	setFiles: ModifyState<NamedPath[]>
 }
 
-export function useSingleInstance({ files: _files, setFiles: _setFiles }: UseSingleInstanceProps) {
+export function useSingleInstance({ files, setFiles }: UseSingleInstanceProps) {
 	const { t } = useTranslation()
+	const navigate = useNavigate()
 
 	async function handleSingleInstance() {
 		await listen<string[]>('single-instance', async (event) => {
 			const argv = event.payload
-			console.log('argv => ', argv)
 			let action = argv?.[1]
 
 			// vibe://download/?url=google.com
@@ -24,8 +26,22 @@ export function useSingleInstance({ files: _files, setFiles: _setFiles }: UseSin
 				const url = params.get('url')
 				if (url) {
 					const hostname = new URL(url).hostname
-					await ask(`${t('common.ask-for-download-model')} ${hostname}?`, { title: t('common.download-model'), kind: 'info' })
+					const confirm = await ask(`${t('common.ask-for-download-model')} ${hostname}?`, { title: t('common.download-model'), kind: 'info' })
+					if (confirm) {
+						console.log('downloading from ', url)
+						navigate('/setup', { state: { downloadURL: url } })
+					}
 				}
+			}
+			// if not action, probably it's open with action
+			const newFiles: NamedPath[] = []
+			for (const arg of argv) {
+				if (config.audioExtensions.some((e) => arg.endsWith(e)) || config.videoExtensions.some((e) => arg.endsWith(e))) {
+					newFiles.push(await pathToNamedPath(arg))
+				}
+			}
+			if (newFiles) {
+				setFiles([...files, ...newFiles])
 			}
 		})
 	}
