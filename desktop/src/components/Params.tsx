59c9ae07d1ede45dbe5e6bc0074659f15befbe00
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-07-11 18:37:13 +0300

    fix build error

--- desktop/src/components/Params.tsx
+++ desktop/src/components/Params.tsx
@@ -44,6 +44,7 @@ export default function ModelOptions({ options, setOptions }: ParamsProps) {
 		})
 	}
 
+	//@ts-ignore
 	async function onRecognizeSpeakerChange(event: ChangeEvent<HTMLInputElement>) {
 		const enabled = event.target.checked
 		if (enabled) {
