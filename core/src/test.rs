d4c373ff6b395692c2baf59d7b75fe6af99ec922
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-01 18:05:15 +0300

    simplify test

--- core/src/test.rs
+++ core/src/test.rs
@@ -7,18 +7,19 @@ use crate::{config::TranscribeOptions, transcribe::create_context};
 use serial_test::serial;
 use std::path::PathBuf;
 use tracing_test::traced_test;
-pub use whisper_rs::SegmentCallbackData;
+use std::time::Instant;
+
 
 #[test]
 #[serial]
 #[traced_test]
 fn test_transcribe() {
-    let ctx = create_context(&PathBuf::from("../ggml-tiny.bin"), None).unwrap();
+    let ctx = create_context(&PathBuf::from("../ggml-medium.bin"), None).unwrap();
     let options = &TranscribeOptions {
         init_prompt: None,
         lang: Some("en".into()),
         max_sentence_len: None,
-        path: "../samples/short.wav".into(),
+        path: "../samples/single.wav".into(),
         verbose: None,
         max_text_ctx: None,
         n_threads: None,
@@ -26,45 +27,8 @@ fn test_transcribe() {
         translate: None,
         word_timestamps: None,
     };
+    let start = Instant::now();
     let result = crate::transcribe::transcribe(&ctx, options, None, None, None, None);
     println!("{:?}", result);
-}
-
-#[test]
-#[serial]
-#[traced_test]
-fn test_transcribe_with_callbacks() {
-    let ctx = create_context(&PathBuf::from("../ggml-tiny.bin"), None).unwrap();
-    let options = &TranscribeOptions {
-        init_prompt: None,
-        lang: Some("en".into()),
-        max_sentence_len: None,
-        path: "../samples/short.wav".into(),
-        verbose: None,
-        max_text_ctx: None,
-        n_threads: None,
-        temperature: None,
-        translate: None,
-        word_timestamps: None,
-    };
-
-    let progress_callback = |progress: i32| {
-        tracing::debug!("desktop progress is {}", progress);
-        println!("{}%", progress);
-    };
-
-    let new_segment_callback = move |data: SegmentCallbackData| {
-        println!("{:?}", data);
-    };
-
-    let result = crate::transcribe::transcribe(
-        &ctx,
-        options,
-        Some(Box::new(progress_callback)),
-        Some(Box::new(new_segment_callback)),
-        None,
-        None,
-    )
-    .unwrap();
-    println!("{:?}", result);
+    println!("Elapsed time: {:.2} seconds", Instant::now().duration_since(start).as_secs_f64());
 }
