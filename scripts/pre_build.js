d65aaa562f92196d10176ea3f2bffaf27a5f4410
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-12 04:46:44 +0300

    try to fix build

--- scripts/pre_build.js
+++ scripts/pre_build.js
@@ -277,11 +277,6 @@ if (process.env.GITHUB_ENV) {
 		await fs.appendFile(process.env.GITHUB_ENV, embed_metal)
 	}
 	if (platform == 'windows') {
-		// ort + whisper.cpp + fbank-rs... something there requires static linking of msvc
-		const rustFlags = `RUSTFLAGS=-C target-feature=+crt-static\n`
-		console.log('Adding ENV', rustFlags)
-		await fs.appendFile(process.env.GITHUB_ENV, rustFlags)
-
 		const openblas = `OPENBLAS_PATH=${exports.openBlas}\n`
 		console.log('Adding ENV', openblas)
 		await fs.appendFile(process.env.GITHUB_ENV, openblas)
