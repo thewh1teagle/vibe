e789df743dce4c3a99eb1a7cbd2278b730dfd8f4
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-26 03:15:05 +0300

    more logging

--- desktop/src-tauri/src/setup.rs
+++ desktop/src-tauri/src/setup.rs
@@ -102,14 +102,17 @@ pub fn setup(app: &App) -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(not(all(any(target_arch = "x86", target_arch = "x86_64"), target_os = "windows")))]
     tracing::debug!("CPU feature detection is not supported on this architecture.");
 
-    tracing::debug!("COMMIT_HASH: {}", env!("COMMIT_HASH"));
+    tracing::debug!("APP VERSION: {}", app.package_info().version.to_string());
+    tracing::debug!("COMMIT HASH: {}", env!("COMMIT_HASH"));
 
     let app_handle = app.app_handle().clone();
     if is_cli_detected() {
+        tracing::debug!("CLI mode");
         tauri::async_runtime::spawn(async move {
             cli::run(&app_handle).await.map_err(|e| eyre!("{:?}", e)).log_error();
         });
     } else {
+        tracing::debug!("Non CLI mode");
         // Create main window
         let result = tauri::WebviewWindowBuilder::new(app, "main", tauri::WebviewUrl::App("index.html".into()))
             .inner_size(800.0, 700.0)
