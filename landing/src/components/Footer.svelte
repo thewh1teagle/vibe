8c5ee6b8d5a24a8bfff4e5b9591df4c33e63910f
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-12-18 03:14:05 +0200

    fix footer

--- landing/src/components/Footer.svelte
+++ landing/src/components/Footer.svelte
@@ -15,29 +15,21 @@
 	<nav class="grid grid-flow-col gap-4">
 		<a class="link link-hover" href={base}>{t('home')}</a>
 
-		<li>
 			<a class="link link-hover" href={`${base}/features`}>{t('features')}</a>
-		</li>
-		<li>
 			<a href="/vibe/docs">{t('documentation')}</a>
-		</li>
 
-		<li>
 			<button
 				on:click={() => {
 					// @ts-ignore
 					window?.['kofi-dialog'].showModal()
 				}}
 				class="link link-hover">{t('support-vibe')}</button>
-		</li>
-		<li>
 			<button
 				on:click={() => {
 					// @ts-ignore
 					window['privacy-policy-modal']?.showModal()
 				}}
 				class="link link-hover">{t('privacy-policy')}</button>
-		</li>
 	</nav>
 	<nav>
 		<div class="grid grid-flow-col gap-4">
