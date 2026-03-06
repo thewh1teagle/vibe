b398b022f7559a9c57cbdc16404625c69f49bd6f
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2026-03-06 04:07:45 +0200

    fix build error

--- desktop/src-tauri/src/cmd/permissions.rs
+++ desktop/src-tauri/src/cmd/permissions.rs
@@ -1,11 +1,22 @@
 #[tauri::command]
 pub async fn request_system_audio_permission() -> bool {
-    tokio::task::spawn_blocking(cpal::request_system_audio_permission)
-        .await
-        .unwrap_or(false)
+    #[cfg(target_os = "macos")]
+    {
+        tokio::task::spawn_blocking(cpal::request_system_audio_permission)
+            .await
+            .unwrap_or(false)
+    }
+
+    #[cfg(not(target_os = "macos"))]
+    {
+        true
+    }
 }
 
 #[tauri::command]
 pub async fn open_system_audio_settings() {
-    cpal::open_system_audio_settings();
+    #[cfg(target_os = "macos")]
+    {
+        cpal::open_system_audio_settings();
+    }
 }
